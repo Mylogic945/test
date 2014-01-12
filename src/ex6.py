83ea9bb2a195e49892eefc00608de6868e8de9e7
Drury Liu <my_logic@sina.com>
2014-01-12 16:38:01 +0800

    initial project version

---
+++ src/ex6.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+x = "There are %d types of people. " % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+print x
+print y
+
+hilarious = False
+joke = "Isn't that joke so funny?! %r"
+
+print joke % hilarious
+
+chinese_charactor = "汉字"
+print "Chinese charactor 汉字 in memory is %r" % chinese_charactor
+
---
+++ src/ex9.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+while True:
+    for i in ["/", "-", "|", "\\", "|"]:
+        print "%s\r" %i, # ending in ','(commas) in order not to print \n.换行  
